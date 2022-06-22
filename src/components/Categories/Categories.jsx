b26f5198c17c3937c8f503cba1478804783fdb7a
Voloshin_Sergey <4voloshin@gmail.com>
2022-06-23 00:14:50 +0300

    feat: added filter for cards

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,14 +1,17 @@
 import styles from './Categories.module.css';
 
-export const Categories = ({ categories }) => {
+export const Categories = ({ categories, activeCategory, categoryHandler }) => {
   return (
     <div className={styles.categories}>
       <div className={styles.categories__active}>
         Show All <img width="10" src={process.env.PUBLIC_URL + '/arrow.svg'} alt="arrow" />
       </div>
       <ul className={styles.categories__list}>
-        {categories.map((category) => (
-          <li key={category.id} className={`${styles.list__item}`}>
+        {categories.map((category, index) => (
+          <li
+            onClick={() => categoryHandler(index)}
+            key={category.id}
+            className={`${styles.list__item} ${activeCategory === index ? styles.active : ''}`}>
             {category.title}
           </li>
         ))}
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,15 +1,52 @@
+import { useState, useEffect } from 'react';
+
 import data from '../../data';
+
 import { Categories } from '../Categories/Categories';
 import { CardList } from '../CardList/CardList';
 
 import styles from './Main.module.css';
 
 export const Main = () => {
+  const [dataCard, setDataCard] = useState([]);
+  const [cardsCount, setCardsCount] = useState(9);
+  const [activeCategory, setActiveCategory] = useState(0);
+
+  useEffect(() => {
+    setDataCard(data.cards);
+  }, []);
+
+  const countHandler = () => {
+    setCardsCount(cardsCount + 9);
+  };
+
+  const categoryHandler = (index) => {
+    if (index === 0) {
+      setCardsCount(9);
+    }
+    setActiveCategory(index);
+  };
+
   return (
     <main className={styles.main}>
-      <Categories categories={data.categories} />
-      <CardList categories={data.categories} cards={data.cards} />
-      <button className={styles.main__btn}>load more</button>
+      <Categories
+        categoryHandler={categoryHandler}
+        activeCategory={activeCategory}
+        categories={data.categories}
+      />
+      <CardList
+        categories={data.categories}
+        cards={dataCard
+          .filter((card) => {
+            if (activeCategory === 0) {
+              return dataCard;
+            } else return card.category === activeCategory;
+          })
+          .slice(0, cardsCount)}
+      />
+      <button onClick={countHandler} className={styles.main__btn}>
+        load more
+      </button>
     </main>
   );
 };
