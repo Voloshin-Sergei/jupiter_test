886007544b737319c983219f01299e7cbbe02a08
Voloshin Sergei <4voloshin@gmail.com>
2022-06-22 15:02:04 +0300

    feat: added Main component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from './components/Header/Header';
+import { Main } from './components/Main/Main';
+
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,9 @@
+import styles from './Main.module.css';
+
+export const Main = () => {
+  return (
+    <main className={styles.main}>
+      <div className={styles.main__container}></div>
+    </main>
+  );
+};
